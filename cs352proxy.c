9031cca2685d5318cf0b2df78fd2d8378be96be1
DCPagan <aphrocraft@gmail.com>
2013-10-13 19:52:57 -0400

    Update cs352proxy.c

--- cs352proxy.c
+++ cs352proxy.c
@@ -1,26 +1,3 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<stdarg.h>
-#include<errno.h>
-#include<fcntl.h>
-#include<pthread.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<net/if.h>
-#include<arpa/inet.h>
-#include<linux/if_tun.h>
-#include<sys/socket.h>
-#include<sys/ioctl.h>
-#include<sys/select.h>
-#include<sys/time.h>
-
-/**************************************************
-  * allocate_tunnel:
-  * open a tun or tap device and returns the file
-  * descriptor to read/write back to the caller
-  *****************************************/
 int allocate_tunnel(char *dev, int flags) {
 	int fd, error;
 	struct ifreq ifr;
